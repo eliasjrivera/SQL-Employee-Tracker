84cc12e2c7d0dbd531979b487dfc4c323eaadf21
Elias Rivera <eliasjohnrivera@gmail.com>
2023-06-27 16:43:10 -0700

    updated server.js and readme

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
 # SQL-Employee-Tracker
+Name
+<br>
+SQL: Employee Tracker
+<br>
+<br>
+Description
+<br>
+When the application is started then you are presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role.
+<br>
+<br>
+When view all departments is chosen then you are presented with formatted table showing department names and department ids.
+<br>
+<br>
+When view all employees is chosen then you are presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to.
+<br>
+<br>
+When add a role is chosen then you are prompted to enter the name, salary, and department for the role and that role is added to the database.
+<br>
+<br>
+When add an employee is chosen then you are prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database. 
+<br>
+<br>
+When update an employee role is chosen then you are prompted to select an employee to update and their new role and this information is updated in the database. 
+<br>
+<br>
+Badges
+<br>
+<br>
+Visuals
+<br>
+![](./gif/app.gif)
+<br>
+<br>
+Project Repo/Screencastify
+<br>
+[Elias Rivera GitHub Repo](https://github.com/eliasjrivera/SQL-Employee-Tracker)
+<br>
+[Screencastify]()
+<br>
+<br>
+Installation
+<br>
+The user should clone the repository from GitHub. This application requires Node.js, Inquirer, console.table and mysql2. To start application run `npm start`. To view database from MySQL `run mysql -u root -p`. 
+<br>
+<br>
+Usage
+<br> 
+This application will allow users to view, add, and edit employees, roles, departments, and managers. 
+<br>
+<br>
+Support
+<br>
+Elias Rivera
+eliasjohnrivera@gmail.com
+<br>
+<br>
+Roadmap
+<br>
+<br>
+Contributing
+<br>
+Contributors should read the installation section.
+<br>
+<br>
+Authors and Acknowledgment 
+<br>
+Elias Rivera
+<br>
+<br>
+License
+<br>
+<br>
+Project Status 
+<br>
+Project is finished. 
--- server.js
+++ server.js
@@ -1,79 +1,178 @@
-// import and require inquirer and mysql2
+// import/require packages
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-
+const cTable = require('console.table')
 require('dotenv').config();
 
 // connect to database
 const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    user: 'root',
-    password: process.env.DB_PASSWORD,
-    database: 'tracker_db'
-  },
-  console.log(`Connected to the tracker_db database.`)
+    {
+        host: 'localhost',
+        user: 'root',
+        password: process.env.DB_PASSWORD,
+        database: 'tracker_db'
+    },
+    console.log(`Connected to the tracker_db database.`)
 );
 
-// inquirer prompt with answer choices and callbacks
-inquirer
-    .prompt([
-        {
-            type: 'list',
-            message: 'What would you like to do?',
-            name: 'Start',
-            choices: [
-                'View All Departments',
-                'View All Roles',
-                'View All Employees',
-                'Add A Department',
-                'Add A Role',
-                'Add An Employee',
-                'Update An Employee Role'
-            ]
-        },
-])
-    .then((data) => {
-      const answers = data;
+// inquirer prompt and switch statment with code blocks to be executed
+const init = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'What would you like to do?',
+                name: 'start',
+                choices: [
+                    'View All Departments',
+                    'View All Roles',
+                    'View All Employees',
+                    'Add A Department',
+                    'Add A Role',
+                    'Add An Employee',
+                    'Update An Employee Role',
+                    'Exit'
+                ]
+            }
+        ]).then(ans => {
+            switch (ans.start) {
+                case 'View All Departments': showDepartment();
+                    break;
+                case 'View All Roles': showRoles();
+                    break;
+                case 'View All Employees': showEmployees();
+                    break;
+                case 'Add A Department': addDepartment();
+                    break;
+                case 'Add A Role': addRole();
+                    break;
+                case 'Add An Employee': addEmployee();
+                    break;
+                case 'Update An Employee Role': updateEmployee();
+                    break;
+                case 'Exit':
+                    console.log('Thank you!');
+                    process.exit();
+            }
+        }).catch(err => console.error(err));
+}
 
-      if (answers === 'View All Departments') {
-        viewDepartments();
-      } 
+init();
 
-      if (answers === 'View All Roles') {
-        viewRoles();
-      }
-
-      if (answers === 'View All Employees') {
-        viewEmployees();
-      }
-
-      if (answers === 'Add A Department') {
-        addDepartment();
-      }
-
-      if (answers === 'Add A Role') {
-        addRole();
-      }
-
-      if (answers === 'Add An Employee') {
-        addEmployee();
-      }
-
-      if (answers === 'Update An Employee Role') {
-        updateEmployee();
-      }
-  });
-
-
-viewDepartments = () => {
-    db.query('SELECT department.id AS id, department.name AS name FROM department', function (err, results) {
-      console.log(results);
+// shows all departments
+const showDepartment = () => {
+    db.query(`SELECT * FROM department`, (err, results) => {
+        err ? console.error(err) : console.table(results);
+        init();
     })
 };
-viewRoles = () => {};
-viewEmployees = () => {};
-addDepartment = () => {};
-addRole = () => {};
-addEmployee = () => {};
-updateEmployee = () => {};
+
+// shows all roles
+const showRoles = () => {
+    db.query(`SELECT * FROM role`, (err, results) => {
+        err ? console.error(err) : console.table(results);
+        init();
+    })
+};
+
+// shows all employees
+const showEmployees = () => {
+    db.query(`SELECT * FROM employee`, (err, results) => {
+        err ? console.error(err) : console.table(results);
+        init();
+    })
+};
+
+// adds a department
+const addDepartment = () => {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is the name of the department you'd like to add?",
+                name: "addDepartment"
+            }
+        ]).then(ans => {
+            db.query(`INSERT INTO department(name)
+                    VALUES(?)`, ans.addDepartment, (err, results) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    db.query(`SELECT * FROM department`, (err, results) => {
+                        err ? console.error(err) : console.table(results);
+                        init();
+                    })
+                }
+            }
+            )
+        })
+};
+
+// adds a role
+const addRole = () => {
+    const departmentChoices = () => db.promise().query(`SELECT * FROM department`)
+        .then((rows) => {
+            let arrNames = rows[0].map(obj => obj.name);
+            return arrNames
+        })
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is the title of the role you'd like to add?",
+                name: "roleTitle"
+            },
+            {
+                type: "input",
+                message: "What is the salary for this role?",
+                name: "roleSalary"
+            },
+            {
+                type: "list",
+                message: "Which department is this role in?",
+                name: "addDepartment",
+                choices: departmentChoices
+            }
+        ]).then(ans => {
+            db.promise().query(`SELECT id FROM department WHERE name = ?`, ans.addDepartment)
+                .then(answer => {
+                    let mappedId = answer[0].map(obj => obj.id);
+                    return mappedId[0]
+                })
+                .then((mappedId) => {
+                    db.promise().query(`INSERT INTO role(title, salary, department_id)
+                VALUES(?, ?, ?)`, [ans.roleTitle, ans.roleSalary, mappedId]);
+                    init()
+                })
+        })
+    };
+
+// adds an employee
+const addEmployee = () => {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is the employee's first name?",
+                name: "firstName"
+            },
+            {
+                type: "input",
+                message: "What is the employee's last name?",
+                name: "lastName"
+            },
+        ]).then(ans => {
+            db.query(`INSERT INTO employee(first_name, last_name)
+                    VALUES(?, ?)`, [ans.firstName, ans.lastName], (err, results) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    db.query(`SELECT * FROM employee`, (err, results) => {
+                        err ? console.error(err) : console.table(results);
+                        init();
+                    })
+                }
+            }
+            )
+        })
+};
